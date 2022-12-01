6a1867a735f7d00d429fc0b928ec30b3ffd67776
listok195 <listok195@gmail.com>
2022-12-01 03:42:20 +0300

    f

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,4 @@
+<script setup>import { useWindowSize } from 'vue-window-size';</script>
 <template>
   <nav>
     <div class="links">
@@ -6,66 +7,187 @@
       <router-link to="/contact">Contact</router-link>
     </div>
     <div class="logo">
-        <router-link to="/"><small>shevchenko mariia</small></router-link>
+      <router-link to="/"><small>shevchenko mariia</small></router-link>
     </div>
-    <div class="socials">
+    <div class="socials fullsc">
       <a href="#"><font-awesome-icon icon="fa-brands fa-pinterest" /></a>
       <a href="#"><font-awesome-icon icon="fa-brands fa-instagram" /></a>
       <a href="#"><font-awesome-icon icon="fa-brands fa-linkedin" /></a>
     </div>
+    <div class=menubtn @click="(polmenu = !polmenu); rotate($event.target)" id=menubtn></div>
   </nav>
-  <router-view/>
+  <div class=mobmenu v-if='(polmenu == 1 && windowHeight < 550)'>
+    <ul>
+      <li>
+        <router-link to="/work">Work</router-link>
+      </li>
+      <li>
+        <router-link to="/about">About</router-link>
+      </li>
+      <li>
+        <router-link to="/contact">Contact</router-link>
+      </li>
+      <div class="socials">
+        <a href="#"><font-awesome-icon icon="fa-brands fa-pinterest" /></a>
+        <a href="#"><font-awesome-icon icon="fa-brands fa-instagram" /></a>
+        <a href="#"><font-awesome-icon icon="fa-brands fa-linkedin" /></a>
+      </div>
+    </ul>
+  </div>
+  <router-view />
 </template>
 
 <script>
-
 export default {
-  name: "App"
+  name: "App",
+  setup() {
+    const { width, height } = useWindowSize();
+    return {
+      windowWidth: width,
+      windowHeight: height,
+    };
+  },
+  data() {
+    return {
+      polmenu: false,
+    }
+  },
+  methods: {
+    rotate(e) {
+      if (this.polmenu == false) {
+        e.classList.remove("rotate");
+      }
+      else {
+        e.classList.add("rotate");
+      }
+    }
+  },
 }
+
 </script>
 
 <style lang="scss">
-  body{
-    margin: 0;
-    overflow-x: hidden;
+body {
+  margin: 0;
+  overflow-x: hidden;
+}
+
+.rotate {
+  transform: rotate(90deg);
+  transition: 300ms;
+}
+
+.menubtn {
+  display: none;
+  background-image: url(./assets/menu-svgrepo-com.svg);
+  width: 20px;
+  height: 20px;
+  transition: 300ms;
+}
+
+ul {
+  list-style: none;
+  padding: 5px;
+
+  span {
+    color: black
   }
-  nav{
-    background-color: #fde8e1;
-    height: 50px;
-    border-bottom: 1px solid black;
-    display: flex;
-    justify-content: space-around;
-    align-items:center;
-    color: $darkgreen;
-  }
-  .links > *{
-    font-size: 15px;
+
+  li {
+    margin: 5px;
     padding: 10px;
-    font-family: $mainFont;
-    text-decoration: none;
+    border: 1px solid $white-color;
+    border-radius: 5px;
+  }
+
+  li:hover {
+    cursor: pointer;
+    filter: brightness(40%)
+  }
+
+  ul:hover {
+    cursor: pointer;
+  }
+}
+
+@keyframes anim {
+  from {
+    right: -40%
+  }
+
+  to {
+    right: 0;
+  }
+
+}
+
+.mobmenu {
+  animation: anim 500ms;
+  background-color: #fde8e1;
+  position: absolute;
+  right: 0px;
+  width: 40%;
+}
+
+nav {
+  background-color: #fde8e1;
+  height: 50px;
+  border-bottom: 1px solid black;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: $darkgreen;
+}
+
+.links>*,
+router-link {
+  font-size: 15px;
+  padding: 10px;
+  font-family: $mainFont;
+  text-decoration: none;
+  color: $darkgreen;
+  text-transform: uppercase;
+
+  &:hover {
+    color: white;
+  }
+}
+
+.logo {
+  &>* {
     color: $darkgreen;
-    text-transform: uppercase;
-    &:hover{
+    text-decoration: none;
+
+    &:hover {
       color: white;
     }
   }
-  .logo {
-    & > * {
-      color: $darkgreen;
-      text-decoration: none;
-      &:hover{
-        color:white;
-      }
-    }
-    small{
-      letter-spacing: 10px;
-    }
+
+  small {
+    letter-spacing: 10px;
+  }
+}
+
+.socials>a {
+  color: $darkgreen;
+  margin: 0 10px;
+  font-size: 17px;
+}
+
+@media screen and (max-width:550px) {
+  .menubtn {
+    display: inline
   }
 
-  .socials > a{
-    color: $darkgreen;
-    margin: 0 10px;
-    font-size: 17px;
+  .fullsc,
+  .links {
+    display: none
   }
 
+  nav {
+    justify-content: space-around
+  }
+}
+
+;
 </style>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="contact">
-    Contact me
+    <h2>Thanks for taking the time to reach out. <br> How can I help you today?</h2>
+    <div class=input-cont>
+      <div class='input-cont-sec'>
+        <div class="text-field">
+          <label class="text-field__label" for="username">Name</label>
+          <input class="text-field__input text-field__input_valid" type="text" name="firstname">
+          <div class="text-field__message"></div>
+        </div>
+        <div class="text-field">
+          <label class="text-field__label" for="username">E-mail</label>
+          <input class="text-field__input text-field__input_valid" type="email" name="firstname">
+          <div class="text-field__message"></div>
+        </div>
+      </div>
+      <div class="textarea-field">
+        <label class="text-field__label" for="username">Massage</label>
+        <textarea class="text-field__input text-field__input_valid" type="text" name="firstname"></textarea>
+        <div class="text-field__message"></div>
+      </div>
+    </div>
+    <button class=subbtn>Submit</button>
   </div>
 </template>
 
@@ -11,3 +31,122 @@ export default {
   name: "ContactView",
 }
 </script>
+<style lang="scss">
+body {
+  background-color: $black-color ;
+}
+
+.input-cont {
+  display: flex;
+  width: 70%;
+}
+
+.input-cont-sec {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 45px;
+  align-items: center;
+}
+
+.input-cont {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.contact {
+  label {
+    font-weight: 700px;
+    font-size: 23px;
+  }
+
+  .subbtn {
+    margin: 40px 0 0 0;
+    color: $white-color;
+    border-color: $white-color;
+    border-radius: 40px;
+    background-color: rgba(255, 255, 255, 0);
+    padding: 20px 90px;
+    font-size: 25px;
+    font-weight: 500;
+  }
+
+  .text-field,
+  .textarea-field {
+    flex-direction: column;
+    margin-bottom: 1rem;
+    display: flex;
+    align-items: center;
+  }
+
+  .text-field {
+    width: 40%;
+  }
+
+  .textarea-field {
+    width: 100%;
+  }
+
+  .text-field__label {
+    display: block;
+    text-align: center;
+    margin-bottom: 10px;
+  }
+
+  textarea {
+    resize: none;
+    width: 100%;
+    height: 250px;
+    border-radius: 200px;
+  }
+
+  input {
+    border-radius: 60px;
+  }
+
+  /* стили для input */
+  .text-field__input {
+    padding: 30px 70px;
+    color: black;
+    background-color: #fff;
+    outline: none;
+    width: 100%;
+    font-size: 25px;
+  }
+
+  h2 {
+    margin: 40px 0 50px 0;
+    font-size: 50px;
+    font-weight: 500;
+    text-align: center;
+  }
+
+  @media screen and (max-width: 1250px) {
+    .text-field {
+      width: 35% !important;
+    }
+  }
+
+  @media screen and (max-width: 800px) {
+    h2 {
+      font-size: 20px !important;
+    }
+
+    .text-field {
+      width: 85% !important;
+    }
+
+    .input-cont-sec {
+      flex-direction: column;
+    }
+  }
+
+
+  display: flex;
+  color: $white-color;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
